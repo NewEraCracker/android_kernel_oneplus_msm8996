ef60a64409f2cc34dc111189c4be24b1176a7137
NewEraCracker <neweracracker@gmail.com>
2017-02-14 21:37:50 +0000

    Build: Update default toolchain to gcc linaro 5.4.1
    Signed-off-by: NewEraCracker <neweracracker@gmail.com>
    (cherry picked from commit 1628c49cc6ba539fb63245da005cedc48e1f5dbf)
    (cherry picked from commit d40b2839b0429378c9e4c96b754aa4bfbdea85f1)

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ RDIR=$(pwd)
 VER=$(cat "$RDIR/VERSION")
 
 # directory containing cross-compile arm64 toolchain
-TOOLCHAIN=$HOME/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu
+TOOLCHAIN=$HOME/gcc-linaro-5.4.1-2017.01-x86_64_aarch64-linux-gnu
 
 CPU_THREADS=$(grep -c "processor" /proc/cpuinfo)
 # amount of cpu threads to use in kernel make process
